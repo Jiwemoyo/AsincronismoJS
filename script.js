f4eccf825fc24cbc96e8f1409e94affc317690ae
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-12-29 12:12:16 -0500

    tomorrow

---
+++ script.js
@@ -0,0 +1,7 @@
+try {
+    //Algo que podria fallar
+} catch (error) {
+    //Capturamos el error de lo que podia fallar y damos una solucion
+}finally{
+    //Esto se ejecuta siempre falle o no falle
+}
