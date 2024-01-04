2e39f19d65f17af87daca206794912fd5f49fa42
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2024-01-04 13:47:24 -0500

    Fetch,APIs,JSON

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
-let prometiendo = new Promise((resolve) => {
-    setTimeout(() => {
-        let objeto = {
-            status:200,
-            description:'Interfaz Cargada con exito'
-    
-        }
-        resolve(objeto)
-    }, 4000);
+let url ='https://jsonplaceholder.typicode.com'
+//GET
+fetch(`${url}/posts`)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+//POST
+
+fetch('https://jsonplaceholder.typicode.com/posts',{
+    method:'POST',
+    body:JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    }),
+    headers:{
+        'Content-type': 'application/json; charset=UTF-8',
+    }
 })
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+//PUT
 
-let prometiendo1 = new Promise((resolve) => {
-    setTimeout(() => {
-        let objeto = {
-            status:200,
-            description:'Login cargado con exito'
-    
-        }
-        resolve(objeto)
-    }, 3000);
-})
-
-let prometiendo2 = new Promise((resolve) => {
-    setTimeout(() => {
-        let objeto = {
-            status:200,
-            description:'Api Cargada con exito'
-    
-        }
-        resolve(objeto)
-    }, 6000);
-})
-
-
-const llamandoPromesa = async()=>{
-    let promesa1 = await prometiendo
-    console.log(promesa1.description)
-
-    let promesa2 = await prometiendo1
-    console.log(promesa2.description)
-
-    let promesa3 = await prometiendo2
-    console.log(promesa3.description)
+let creando ={
+    id:1,
+    title: 'fobia',
+    body: 'bar',
+    userId: 4,
 }
 
+fetch('https://jsonplaceholder.typicode.com/posts/1',{
+    method:'PUT',
+    body:JSON.stringify(creando),
+    headers:{
+        'Content-type': 'application/json; charset=UTF-8',
+    }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+//PATCH
+fetch('https://jsonplaceholder.typicode.com/posts/1',{
+    method:'PATCH',
+    body:JSON.stringify({
+        title: 'fobiandores',
+    }),
+    headers:{
+        'Content-type': 'application/json; charset=UTF-8',
+    }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+//DELETE
 
-llamandoPromesa()
+fetch('https://jsonplaceholder.typicode.com/posts/1',{
+    method:'DELETE',
+})
